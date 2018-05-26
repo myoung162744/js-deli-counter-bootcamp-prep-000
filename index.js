9c0760bfc51d182f0272b7a48012ebbb654ad763
Matthew Young <myoung162744@gmail.com>
2018-05-26 19:08:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function nowServing(katzDeliLine) {
   katzDeliLine.unshift()
   }
   if (katzDeliLine.length === 0){
-    return "There is nobody waiting to be served!"
+    "There is nobody waiting to be served!"
+    return default
   }
 }
