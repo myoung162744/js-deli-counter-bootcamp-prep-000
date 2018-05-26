f252c74ac5c676b8b095636d6378c43394dc1f79
Matthew Young <myoung162744@gmail.com>
2018-05-26 18:56:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-  var greeting = "Welcome, " + name + ". You are number " + length.katzDeliLine + " in line."
+  var greeting = "Welcome, " + name + ". You are number " + katzDeliLine[name] + " in line."
   return greeting
 }
