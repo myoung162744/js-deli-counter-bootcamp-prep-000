b2ef7bee30b2184403a6be7bdb0f9b78e95534c7
Matthew Young <myoung162744@gmail.com>
2018-05-26 18:54:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  var greeting = "Welcome, " + name +". You are number " + length.katzDeliLine" " in line."
+  return greeting
+}
