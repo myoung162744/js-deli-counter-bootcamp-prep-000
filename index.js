5f02c6d0a03b6d4c8d1376e0cc0a86b79fd6f31f
Matthew Young <myoung162744@gmail.com>
2018-05-26 19:02:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function takeANumber(katzDeliLine, name) {
   var greeting = "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
   return greeting
 }
+
+function nowServing() {
+  while(katzDeliLine.length > 0){
+    return katzDeliLine[0]
+    katzDeliLine.unshift()
+  }
+  return "There is nobody waiting to be served!"
+}
